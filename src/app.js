89f11a99706817d3b811f2ee9da5ae14e52ebcc8
lucciii33 <angelomaiele@gmail.com>
2022-01-20 02:52:52 +0000

    done card

--- src/app.js
+++ src/app.js
@@ -4,8 +4,27 @@ import "./style.css";
 
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
+import { read } from "@popperjs/core";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  let ramdonSuit = Math.floor(Math.random() * 4);
+
+  let ramdonNum = Math.floor(Math.random() * 12);
+
+  let numbers = [2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K", "A"];
+  let suits = ["&#9829", "&#9830", "&#9824", "&#9827"];
+
+  var card = document.querySelector(".card");
+
+  var suit = (document.querySelector(".icon").innerHTML = suits[ramdonSuit]);
+  var num = (document.querySelector(".num").innerHTML = numbers[ramdonNum]);
+  var suitUpSide = (document.querySelector(".iconreflex").innerHTML =
+    suits[ramdonSuit]);
+
+  if (suit == "&#9829" || suit == "&#9827") {
+    var suit = (document.querySelector(".icon").style.color = "red");
+    var num = (document.querySelector(".num").style.color = "red");
+    var suitUpSide = (document.querySelector(".iconreflex").style.color =
+      "red");
+  }
 };
